067b93a45fe6d1a4633ae50370436805ca95abb5
Hoanh An <hoanhan@bennington.edu>
2020-02-06 11:38:55 -0500

    refactor system route

--- internal/handler/system.go
+++ internal/handler/system.go
@@ -1,31 +1,20 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/labstack/echo/v4"
+
+	"github.com/hoanhan101/progress/internal/model"
 )
 
-// System represents a row in system table.
-type System struct {
-	ID          string    `db:"system_id" json:"system_id"`
-	GoalID      string    `db:"goal_id" json:"goal_id"`
-	Name        string    `db:"name" json:"name"`
-	Repeat      string    `db:"repeat" json:"repeat"`
-	DateCreated time.Time `db:"date_created" json:"date_created"`
-}
-
-// ListSystems list all systems.
+// ListSystems is the handler for listing all systems endpoint.
 func (h *Handler) ListSystems(c echo.Context) error {
-	systems := []System{}
-	const q = `SELECT * FROM systems;`
-
-	if err := h.db.Select(&systems, q); err != nil {
+	systems, err := model.ListSystems(h.db)
+	if err != nil {
 		return echo.NewHTTPError(
 			http.StatusInternalServerError,
-			fmt.Sprintf("failed to select: %v", err),
+			err.Error(),
 		)
 	}
 
---
+++ internal/model/system.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"time"
+
+	"github.com/jmoiron/sqlx"
+)
+
+// System represents a row in system table.
+type System struct {
+	ID          string    `db:"system_id" json:"system_id"`
+	GoalID      string    `db:"goal_id" json:"goal_id"`
+	Name        string    `db:"name" json:"name"`
+	Repeat      string    `db:"repeat" json:"repeat"`
+	DateCreated time.Time `db:"date_created" json:"date_created"`
+}
+
+// ListSystems get all systems in the table.
+func ListSystems(db *sqlx.DB) ([]System, error) {
+	systems := []System{}
+	const q = `SELECT * FROM systems;`
+
+	if err := db.Select(&systems, q); err != nil {
+		return nil, err
+	}
+
+	return systems, nil
+}
