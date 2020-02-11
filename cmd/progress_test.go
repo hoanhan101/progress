81a0bfb761a5b88fb66bc6bf4daafb2e9e675f62
Hoanh An <hoanhan101@gmail.com>
2020-02-11 16:45:29 -0500

    add /goal e2e test

--- cmd/progress_test.go
+++ cmd/progress_test.go
@@ -14,15 +14,14 @@ import (
 	"gopkg.in/resty.v1"
 
 	"github.com/hoanhan101/progress/internal/config"
+	"github.com/hoanhan101/progress/internal/model"
 )
 
-const (
-	host = "http://localhost:8000"
-)
+var request = resty.New().SetHostURL("http://localhost:8000").R()
 
 func TestConfig(t *testing.T) {
 	cfg := new(config.Config)
-	_, err := resty.New().SetHostURL(host).R().SetResult(cfg).Get("/config")
+	_, err := request.SetResult(cfg).Get("/config")
 	assert.NoError(t, err)
 	assert.Equal(t, ":8000", cfg.Server.Address)
 	assert.Equal(t, "postgres", cfg.Database.User)
@@ -36,8 +35,58 @@ func TestHealth(t *testing.T) {
 	health := &struct {
 		Status string
 	}{}
-
-	_, err := resty.New().SetHostURL(host).R().SetResult(health).Get("/health")
+	_, err := request.SetResult(health).Get("/health")
 	assert.NoError(t, err)
 	assert.Equal(t, "ok", health.Status)
+}
+
+func TestGoal(t *testing.T) {
+	goals := new([]model.Goal)
+	_, err := request.SetResult(goals).Get("/goal")
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(*goals))
+
+	newGoal := new(model.Goal)
+	_, err = request.SetBody(map[string]interface{}{"name": "foo"}).SetResult(newGoal).Post("/goal")
+	assert.NoError(t, err)
+	assert.NotNil(t, newGoal.ID)
+	assert.Equal(t, "foo", newGoal.Name)
+	assert.NotNil(t, newGoal.DateCreated)
+
+	_, err = request.SetResult(goals).Get("/goal")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(*goals))
+
+	for _, g := range *goals {
+		assert.Equal(t, newGoal.ID, g.ID)
+		assert.Equal(t, newGoal.Name, g.Name)
+		assert.NotNil(t, newGoal.DateCreated)
+	}
+
+	goal := new(model.Goal)
+	_, err = request.SetResult(goal).Get("/goal/" + newGoal.ID)
+	assert.NoError(t, err)
+	assert.Equal(t, newGoal.ID, goal.ID)
+	assert.Equal(t, newGoal.Name, goal.Name)
+	assert.NotNil(t, newGoal.DateCreated)
+
+	putGoal := new(model.Goal)
+	_, err = request.SetBody(map[string]interface{}{"name": "bar"}).SetResult(putGoal).Put("/goal/" + newGoal.ID)
+	assert.NoError(t, err)
+	assert.NotNil(t, putGoal.ID)
+	assert.Equal(t, "bar", putGoal.Name)
+	assert.NotNil(t, putGoal.DateCreated)
+	assert.NotEqual(t, newGoal.Name, putGoal.Name)
+	assert.Equal(t, newGoal.ID, putGoal.ID)
+
+	status := &struct {
+		Message string
+	}{}
+	_, err = request.SetResult(status).Delete("/goal/" + newGoal.ID)
+	assert.NoError(t, err)
+	assert.Equal(t, "deleted successfully", status.Message)
+
+	_, err = request.SetResult(goals).Get("/goal")
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(*goals))
 }
